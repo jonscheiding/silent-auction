e4cfa3f78a42a4c2f5e08ef4f71711b1393ac77c
Jon Scheiding <jonscheiding@gmail.com>
2020-04-07 18:11:47 -0400

    Initial POC of inserting bidder information to DB

---
+++ imports/api/bidders.js
@@ -0,0 +1,3 @@
+import { Mongo } from 'meteor/mongo';
+ 
+export const Bidders = new Mongo.Collection('bidders');
--- imports/ui/BidderLoginDialog.jsx
+++ imports/ui/BidderLoginDialog.jsx
@@ -1,6 +1,9 @@
 import React, { useState } from 'react';
+import { useTracker } from 'meteor/react-meteor-data'
 
 import { useBidderEmail } from './bidderEmail';
+
+import { Bidders } from '../api/bidders';
 
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
@@ -10,27 +13,44 @@ export const BidderLoginDialog = ({show, onHide}) => {
   const [bidderEmail, setBidderEmail] = useBidderEmail();
   const [enteredEmail, setEnteredEmail] = useState(bidderEmail);
 
+  const bidderInformation = useTracker(() => 
+    Bidders.findOne({bidderEmail: enteredEmail}));
+
   const handleEmailChange = (e) => setEnteredEmail(e.target.value);
   
   const handleValidateClick = (e) => {
+    Bidders.insert({
+      bidderEmail: enteredEmail
+    });
+
     setBidderEmail(enteredEmail);
     e.preventDefault();
   }
 
+  const bidderEmailPrompt = (
+    <>
+      <p>In order to bid, you must provide a valid e-mail address. We will contact you using this e-mail address if you win.</p>
+      <Form>
+        <Form.Group>
+          <Form.Control type="email" 
+            value={enteredEmail} onChange={handleEmailChange}
+            placeholder="Email address" />
+        </Form.Group>
+        <Button variant="primary" onClick={handleValidateClick} type="submit">
+          Validate
+        </Button>
+      </Form>
+    </>
+  );
+
   return (
     <Modal show={show} onHide={onHide}>
       <Modal.Body>
-        <p>In order to bid, you must provide a valid e-mail address. We will contact you using this e-mail address if you win.</p>
-        <Form>
-          <Form.Group>
-            <Form.Control type="email" 
-              value={enteredEmail} onChange={handleEmailChange}
-              placeholder="Email address" />
-          </Form.Group>
-          <Button variant="primary" onClick={handleValidateClick} type="submit">
-            Validate
-          </Button>
-        </Form>
+        {
+          bidderInformation === undefined
+            ? bidderEmailPrompt
+            : <div>Pending</div>
+        }
       </Modal.Body>
     </Modal>
   );
