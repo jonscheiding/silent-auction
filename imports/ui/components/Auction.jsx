7496c032c1e10b7b75533551bd2fda673ddaa5b2
Jon Scheiding <jonscheiding@gmail.com>
2020-04-29 21:29:20 -0400

    Make navigation work right with grouped/sorted items

--- imports/ui/components/Auction.jsx
+++ imports/ui/components/Auction.jsx
@@ -27,25 +27,53 @@ function byArtistAndTitle(a, b) {
   return 0;
 }
 
+function findNeighbor(array, element, increment) {
+  let index = array.findIndex(element);
+  index += increment;
+  while (index < 0) { index += array.length; }
+  while (index >= array.length) { index -= array.length; }
+
+  return array[index];
+}
+
 export const Auction = ({
-  items, bidder, auction, selectedItemId, onSelectItem, onDeselectItem, onNavigateItem,
+  items, bidder, auction, selectedItemId, onSelectItem, onDeselectItem,
 }) => {
   const itemsByCategory = groupBy(items, (item) => item.content.category);
   const categories = Object.keys(itemsByCategory);
 
-  return categories.map((category) => (
-    <Auction.Category
-      key={category}
-      category={category}
-      items={itemsByCategory[category]}
-      bidder={bidder}
-      auction={auction}
-      selectedItemId={selectedItemId}
-      onSelectItem={onSelectItem}
-      onDeselectItem={onDeselectItem}
-      onNavigateItem={onNavigateItem}
-    />
-  ));
+  const flattenedItems = [];
+
+  let onNavigateItem = (direction) => {
+    const nextItem = findNeighbor(
+      flattenedItems,
+      (e) => e._id === selectedItemId,
+      direction,
+    );
+
+    onSelectItem(nextItem);
+  };
+
+  if (onSelectItem == null) { onNavigateItem = null; }
+
+  return categories.map((category) => {
+    const sortedItems = itemsByCategory[category].sort(byArtistAndTitle);
+    flattenedItems.push(...sortedItems);
+
+    return (
+      <Auction.Category
+        key={category}
+        category={category}
+        items={sortedItems}
+        bidder={bidder}
+        auction={auction}
+        selectedItemId={selectedItemId}
+        onSelectItem={onSelectItem}
+        onDeselectItem={onDeselectItem}
+        onNavigateItem={onNavigateItem}
+      />
+    );
+  });
 };
 
 Auction.Category = ({
@@ -57,7 +85,7 @@ Auction.Category = ({
       <Col><h4>{category}</h4></Col>
     </Row>
     <Row>
-      {items.sort(byArtistAndTitle).map((item) => (
+      {items.map((item) => (
         <Col md={6} lg={4} key={item._id}>
           <AuctionItem
             item={item}
