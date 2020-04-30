856df16d0df5c18bc54038398913e6b6a53f4101
Jon Scheiding <jonscheiding@gmail.com>
2020-04-29 21:11:41 -0400

    Make the jumbotron collapsible

--- imports/ui/components/Splash.jsx
+++ imports/ui/components/Splash.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import Button from 'react-bootstrap/Button';
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Accordion from 'react-bootstrap/Accordion';
 
 import { HtmlContent } from './util/HtmlContent';
 import { useCurrentAuction } from '../hooks/meteor';
@@ -18,8 +20,14 @@ export const Splash = () => {
         <title>{auction.content.title}</title>
       </Helmet>
       <Jumbotron>
-        <h4>Welcome to {auction.content.title}</h4>
-        <HtmlContent html={auction.content.welcomeMessage} />
+        <Accordion defaultActiveKey="0">
+          <Accordion.Toggle as={Button} variant="link" eventKey="0">
+            <h4>Welcome to {auction.content.title}</h4>
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <HtmlContent html={auction.content.welcomeMessage} />
+          </Accordion.Collapse>
+        </Accordion>
       </Jumbotron>
     </>
   );
